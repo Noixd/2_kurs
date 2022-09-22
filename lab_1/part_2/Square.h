118acbe8f8aaddd283488f80293c094bdd6e7621
Noix <100488924+Noixd@users.noreply.github.com>
2022-09-22 20:38:07 +0300

    Update Square.h

--- lab_1/part_2/Square.h
+++ lab_1/part_2/Square.h
@@ -1,65 +1,44 @@
 #include "header.h"
 
+using namespace std;
+
 class Square
 {
-	private:
-		double x1;
-		double y1;
-		int adress = 0;
+private:
+	double x1;
+	double y1;
 
-	public:
+public:
+	Square() {
+		cout << "Create obj " << endl;
+		x1 = 0;
+		y1 = 0;
+	}
 
-		static int i;
+	Square(double inpx, double inpy) {
+		cout << "Create obj " << endl;
+		x1 = inpx;
+		y1 = inpy;
+	}
 
-		Square() { 
+	Square(const Square &copy) {
+		cout << "Copy obj" << endl;
+		x1 = copy.x1;
+		y1 = copy.y1;
+	}
 
-			i++;
-			adress = i;
-			x1 = 0;
-			y1 = 0;
+	~Square() {
+		
+		cout << "Destruct obj" << endl;
+	}
 
-			cout << "Create obj " << adress << endl;
-		}
-
-		Square(double inpx, double inpy) {
-
-			i++;
-			adress = i;
-			x1 = inpx;
-			y1 = inpy;
-
-			cout << "Create obj " << adress << endl;
-		}
-
-		Square(const Square &copy) {
-
-			i++;
-			adress = i;
-			x1 = copy.x1;
-			y1 = copy.y1;
-
-			cout << "Copy obj " << copy.adress << " to " << adress << endl;
-		}
-
-		~Square() {
-			
-			cout << "Destruct obj " << adress << endl;
-		}
-
-		/*int ID(){
-			int s_adress;
-			return s_adress++;
-		}*/
-
-		void SetX() { 
-			cout << "Input X" << endl;
-
-			while (true) {		
+	void SetX() { 
+		while (true) {		
 			
 				cin >> x1;
 
 				if (cin.fail()) {
-
+					
 					cin.clear();
 					cin.ignore(32767,'\n');
 
@@ -67,12 +46,10 @@ class Square
 				}
 				else break;
 			}
-		}
+	}
 
-		void SetY() { 
-			cout << "Input Y" << endl;
-
-			while (true) {		
+	void SetY() { 
+		while (true) {		
 			
 				cin >> y1;
 
@@ -85,29 +62,22 @@ class Square
 				}
 				else break;
 			}
+	}
+
+	void Get() {
+		cout << "Coordinates" << endl << "X: " << x1 << "px" << endl << "Y: " << y1 << "px" << endl;
+	}
+
+	double Area() {
+		return x1 * y1;
+	}
+
+	/*void DI() {
+		int i = 1000;
+		while (i>0)
+		{
+			cout << i << endl;
+			i -= 7;
 		}
-
-		void Get() {
-			cout << "Coordinates of " << adress << " rectanlge" << endl << "X: " << x1 << " px" << endl << "Y: " << y1 << " px" << endl;
-		}
-
-		void get_adr() {
-			cout << adress << endl;
-		}
-
-		void Area() {
-
-			cout << "Area of " << adress << " rectanlge: "; 
-
-			cout << x1 * y1 << endl;
-		}
-
-		/*void DI() {
-			int i = 1000;
-			while (i>0)
-			{
-				cout << i << endl;
-				i -= 7;
-			}
-		}*/
+	}*/
 };
