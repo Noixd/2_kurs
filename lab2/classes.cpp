02df17d112859b47532cc5ab523a794d8f609715
Noix <100488924+Noixd@users.noreply.github.com>
2022-10-08 22:44:49 +0300

    Update classes.cpp

--- lab2/classes.cpp
+++ lab2/classes.cpp
@@ -1,10 +1,10 @@
 #include "header.h"
 
-class Square
-{		
-	public:
+class Square		//базовый класс
+{		int adress = 0;
+	protected:
 		double x1, y1;
-		int adress = 0;
+	public:
 		static int i;
 
 		Square() { 
@@ -44,27 +44,29 @@ class Square
 
 
 		void SetX() { 
+		
 			cout << "Input X1" << endl;
 
-			while (true) {		
+			while (true) {
 			
 				cin >> x1;
 
 				if (cin.fail()) {
-
+					
 					cin.clear();
 					cin.ignore(32767,'\n');
 
 					cout << "Incorrect value" << endl;
-				}
-				else break;
+			}
+			else break;
 			}
 		}
 
 		void SetY() { 
+			
 			cout << "Input Y1" << endl;
 
-			while (true) {		
+			while (true) {
 			
 				cin >> y1;
 
@@ -74,17 +76,13 @@ class Square
 					cin.ignore(32767,'\n');
 
 					cout << "Incorrect value" << endl;
-				}
-				else break;
+			}
+			else break;
 			}
 		}
 
 		void Get() {
-			cout << "Coordinates of " << adress << " rectanlge" << endl << "X: " << x1 << " px" << endl << "Y: " << y1 << " px" << endl;
-		}
-
-		void get_adr() {
-			cout << adress << endl;
+			cout << "Coordinates of " << adress << " point" << endl << "X: " << x1 << " px" << endl << "Y: " << y1 << " px" << endl;
 		}
 
 		void Area() {
@@ -95,20 +93,21 @@ class Square
 		}
 };
 
-class Line : public Square {
+class Line : public Square {	//производный класс с модификатором public
 
 	private:
 		double x2, y2;
-		int adress; 
+		int adress = 0; 
 
 	public:
 
-		Line(){
+		Line(): Square() {
 			x2 = 0;
 			y2 = 0;	 
 		}
 
 		void SetX2() { 
+
 			cout << "Input X2" << endl;
 
 			while (true) {		
@@ -116,20 +115,21 @@ class Line : public Square {
 				cin >> x2;
 
 				if (cin.fail()) {
-
+					
 					cin.clear();
 					cin.ignore(32767,'\n');
 
 					cout << "Incorrect value" << endl;
-				}
-				else break;
+			}
+			else break;
 			}
 		}
 
 		void SetY2() { 
+			
 			cout << "Input Y2" << endl;
 
-			while (true) {		
+			while (true) {
 			
 				cin >> y2;
 
@@ -139,16 +139,21 @@ class Line : public Square {
 					cin.ignore(32767,'\n');
 
 					cout << "Incorrect value" << endl;
-				}
-				else break;
+			}
+			else break;
 			}
 		}
 
-		void lenght () {
+		void Get1() {
+
+			Get();
+			cout << "Coordinates of " << adress << " point" << endl << "X: " << x2 << " px" << endl << "Y: " << y2 << " px" << endl;
+		}
+
+		void lenght() {	// метод рассчёта расстояния от точки до точки
 
 			double line_segment_length = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 
 			cout << "Lenght of line: " << line_segment_length << endl;
 		}
-
 };
