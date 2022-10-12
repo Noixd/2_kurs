0e853ae1c6e4d5ff3156a6d6cf6385b2b07a2e31
Noix <100488924+Noixd@users.noreply.github.com>
2022-10-12 12:48:34 +0300

    Create main.cpp

---
+++ lab2/lab(ext)/main.cpp
@@ -0,0 +1,24 @@
+#include "header.h"
+#include "classes.cpp"
+
+int Square :: i;
+
+int main(){
+
+	
+    setlocale(LC_ALL, "ru");
+
+		Line ln1;
+		Square sq1;
+
+		sq1.SetX();
+		sq1.SetY();
+		ln1.Square::SetX();
+		ln1.Square::SetY();
+
+		ln1.Get();
+
+		ln1.lenght(sq1);
+		
+	return 0;
+}
