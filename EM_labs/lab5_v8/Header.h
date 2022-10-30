a197f81d52aaaefd6348e552b4f80af4c3469be4
Noix <100488924+Noixd@users.noreply.github.com>
2022-10-30 17:20:58 +0300

    Create Header.h

---
+++ EM_labs/lab5_v8/Header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER
+#define HEADER
+#define TEST_MESS
+
+#include <iostream>
+#include <fstream>
+#include <String>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+#endif
