92d9e55acdba4881fcb2b3f06637afb7899e7402
Noix <100488924+Noixd@users.noreply.github.com>
2022-10-30 19:20:56 +0300

    Update main.cpp

--- ТПС/Пункт_3/main.cpp
+++ ТПС/Пункт_3/main.cpp
@@ -21,6 +21,7 @@ int main() {
     double t [32];
     double U_first [32] , U_second [32];
     int i = 0 , C  = 1;
+
     ofstream out1;
     out1.open("Table.txt");
     out1.precision(6);
@@ -28,7 +29,7 @@ int main() {
     while (C < 5) {   
         i = 0;
         if(out1.is_open()) {
-            out1  << "Cycle " << C << endl << endl << "i" << "\tt,s" << "\t\t\tU8" << "\t\t\tU9" << endl;
+            out1  << "Cycle " << C << endl << endl << "i" << "\tt,s" << "\tU8" << "\tU9" << endl;
 
             while (i < 32) {   
                 t[i] = Tk * (i + 1)  + T0 * (C - 1);
@@ -38,7 +39,7 @@ int main() {
 
                     // Формула для второго сигнала
                 U_second [i] = 0.15 * ((3*sin(2 * PI * U_inp_val2 * t [i]) + sin(6 * PI * U_inp_val2 * t[i]))/4);
-
+                                
                 out1 << i << "\t" << t[i] << "\t" << U_first[i] << "\t" << U_second[i] << endl;
                 i++;
             }
