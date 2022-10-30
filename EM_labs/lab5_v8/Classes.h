42d17a29d3c5675f239ce259d7c2574969834e00
Noix <100488924+Noixd@users.noreply.github.com>
2022-10-30 17:21:44 +0300

    Add files via upload

---
+++ EM_labs/lab5_v8/Classes.h
@@ -0,0 +1,22 @@
+
+#include "Header.h"
+
+class Gen_Dir {
+
+    private:
+        list <string> gen_list;
+      
+    public:
+
+        void Add_elem();
+
+        void Get_elem();
+
+        void Sort();
+
+        void Del_elem();
+
+        Gen_Dir();
+
+        ~Gen_Dir();
+};
